28d67f0a6cd039ad96ceb525fb6dcb6b8924d262
Bastian Ruppert <info@bastian-ruppert.de>
2016-05-07 08:05:11 +0200

    ExaktG hinzugefuegt
    Signed-off-by: Bastian Ruppert <info@bastian-ruppert.de>

---
+++ ExaktG.cpp
@@ -0,0 +1,59 @@
+/*
+Bastian Ruppert
+*/
+
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
+
+#include "LL.h"
+#include "Poll.h"
+#include "G_Ctrl.h"
+
+#include "ExaktG.h"
+
+namespace EuMax01
+{
+
+  ExaktG::ExaktG(int verbExakt,int verbG):GCtrl(verbG)
+  {
+    pr_gcodes = new PollReader(this);
+    this->setVerbose(verbExakt); 
+  }
+
+  void ExaktG::setFD(int fd)
+  {
+    this->pr_gcodes->setReadSource(fd,(char*)"ExaktG_Ctrl");
+  }
+
+  void ExaktG::setVerbose(int verb)
+  {
+    this->verbose = verb;    
+  }
+
+  G_Ctrl * ExaktG::getG_Ctrl(void)
+  {
+    return &GCtrl;
+  }
+
+  PollReader * ExaktG::getPollReader(void)
+  {
+    return this->pr_gcodes;
+  }
+
+  void ExaktG::pollReadEvent(PollSource * s)
+  {
+    char buf[1024];
+    int buflen = 1024;
+    buf[1023] = '\n';
+
+    if(read(s->thePollfd.fd,buf,buflen-1)){
+      printf("%s\n",buf);
+    }
+  }
+
+}
---
+++ ExaktG.h
@@ -0,0 +1,31 @@
+/*
+Bastian Ruppert
+*/
+
+namespace EuMax01
+{
+#ifndef __ExaktG_H__
+#define __ExaktG_H__
+
+  class ExaktG:IPollReadListener
+  {
+  public:
+    ExaktG(int verbExakt,int verbGCtrl);
+    ~ExaktG(){
+      delete(pr_gcodes);
+    }
+
+    void setVerbose(int verb);
+    virtual void (pollReadEvent)(PollSource * s);
+    void setFD(int fd);
+    PollReader* getPollReader(void);
+    G_Ctrl * getG_Ctrl(void);
+
+  private:
+    PollReader * pr_gcodes;
+    int verbose;
+    G_Ctrl GCtrl;
+  };
+
+#endif /* __ExaktG_H__*/
+}//end namespace
