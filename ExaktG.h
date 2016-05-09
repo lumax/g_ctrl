aecf2ce690f38e3fb6c44088eab39244d279edc8
Bastian Ruppert <info@bastian-ruppert.de>
2016-05-09 20:56:49 +0200

    ExaktG: jsmn Parser herausgekickt!
    Signed-off-by: Bastian Ruppert <info@bastian-ruppert.de>

--- ExaktG.h
+++ ExaktG.h
@@ -7,8 +7,6 @@ namespace EuMax01
 #ifndef __ExaktG_H__
 #define __ExaktG_H__
 
-#include "jsmn.h"
-
   class ExaktG:IPollReadListener
   {
   public:
@@ -22,15 +20,11 @@ namespace EuMax01
     void setFD(int fd);
     PollReader* getPollReader(void);
     G_Ctrl * getG_Ctrl(void);
-    int jsonParser(char * buffer, int buflen);
 
   private:
     PollReader * pr_gcodes;
     int verbose;
     G_Ctrl GCtrl;
-    jsmn_parser p;
-    jsmntok_t t[128]; /* We expect no more than 128 tokens */
-    int jsoneq(const char *json, jsmntok_t *tok, const char *s);
   };
 
 #endif /* __ExaktG_H__*/
